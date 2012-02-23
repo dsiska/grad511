b284c066dd1634fb28e7917d2d874200742b464a
David S <siskasalkin@gmail.com>
2012-02-22 23:08:37 -0700

    HW5 part 2 roundrobin

--- MPIRR.c
+++ MPIRR.c
@@ -17,11 +17,13 @@ MPI_Comm_size(MPI_COMM_WORLD, &mysize); /*Get no. of processors*/
 
 
 
-printf("Processor %d of %d: Hello World!\n", myid, mysize);
+//printf("Processor %d of %d: Hello World!\n", myid, mysize);
 
 /*compute myid_right and myid_left */
 
-myid_right = myid + 1;
+/*
+
+   myid_right = myid + 1;
 
 if (myid_right >= mysize) { 
 	myid_right = 0; 
@@ -31,35 +33,39 @@ myid_left = 0;
 
 ierr = MPI_Isend(&myid, 1, MPI_INTEGER, myid_right, 0, MPI_COMM_WORLD, &ireq);
 
+*/
+
 /* blocking receive from left neighbor */
+
+/*
 
 ierr = MPI_Recv(&myid_left,1,MPI_INTEGER,MPI_ANY_SOURCE,0,MPI_COMM_WORLD, &istat);
 
-printf("Processor %d mid of left neighbor %d\n", myid, myid_left);
-
+printf("Processor %d right of neighbor %d\n", myid, myid_left);
+ */
 
 
 
 //modify this to also send all off these messages back - i.e. to the left neighbor processor.
-printf("Processor %d of %d: Hello World!\n", myid, mysize);
+//printf("Processor %d of %d: Hello World!\n", myid, mysize);
 
 /*compute myid_right and myid_left */
 
-myid_right = myid + 1;
+myid_left = myid - 1;
 
-if (myid_right >= mysize) { 
-	myid_right = 0; 
+if (myid_left == -1) { 
+	myid_left = 24; 
 	}
 
-myid_left = 0;
+//initialize myid_right - assigned by MPI Recv
+myid_right = 0;
 
-ierr = MPI_Isend(&myid, 1, MPI_INTEGER, myid_right, 0, MPI_COMM_WORLD, &ireq);
+ierr = MPI_Isend(&myid, 1, MPI_INTEGER, myid_left, 0, MPI_COMM_WORLD, &ireq);
 
 /* blocking receive from left neighbor */
+ierr = MPI_Recv(&myid_right,1,MPI_INTEGER,MPI_ANY_SOURCE,0,MPI_COMM_WORLD, &istat);
 
-ierr = MPI_Recv(&myid_left,1,MPI_INTEGER,MPI_ANY_SOURCE,0,MPI_COMM_WORLD, &istat);
-
-printf("Processor %d mid of left neighbor %d\n", myid, myid_left);
+printf("Processor %d left of neighbor %d\n", myid, myid_right);
 
 MPI_Finalize(); /* Terminate MPI */
 
